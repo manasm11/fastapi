802a8516828a04424182b2a9e08355f499ad1f6a
Manas Mishra <manas.m22@gmail.com>
2022-07-05 22:49:13 +0530

    Learned about documenting the api endpoints

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+from typing import Optional
 from fastapi import FastAPI
+from enum import Enum
+
+
+class BlogType(str, Enum):
+    travel = "travel"
+    howto = "howto"
+    philosophy = "philosophy"
+
 
 app = FastAPI()
 
@@ -13,3 +22,31 @@ def test_endpoint():
 def all_blogs():
     """Retrieve all blogs."""
     return {"message": "All blogs"}
+
+
+@app.get("/blog/{id}", tags=["Blog"])
+def blog_with_id(id: int):
+    """Retrieve a specific blog using id."""
+    return {"message": f"Blog with id {id}"}
+
+
+@app.get("/blog/type/{type}", tags=["Blog"])
+def blogs_of_certain_type(type: BlogType):
+    """Retrieve blogs of a certain category"""
+    return {"message": f"All blogs of category {type}"}
+
+
+@app.get("blog/{id}/comment/{comment_id}", tags=["Blog", "Comment"])
+def get_comment(
+    id: int, comment_id: int, valid: bool = True, username: Optional[str] = ""
+):
+    """
+    Retrieve comment of a blog
+    - **id** mandatory blog id
+    - **comment_id** mandatory comment id
+    - **valid** optional valid boolean
+    - **username** optional username
+    """
+    return {
+        "message": f"blog_id {id}, comment_id {comment_id}, valid {valid} username {username}"
+    }
