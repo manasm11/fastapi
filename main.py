eb325df079e6d7a1ff7a65df59895d4e616d39b5
Manas Mishra <manas.m22@gmail.com>
2022-07-05 23:14:23 +0530

    Added blog router

--- main.py
+++ main.py
@@ -1,55 +1,12 @@
-from typing import Optional
-from fastapi import FastAPI, Response
-from enum import Enum
+from fastapi import FastAPI
+from routers import blog_get
 
 
 app = FastAPI()
+app.include_router(blog_get.router)
 
 
 @app.get("/hello")
 def test_endpoint():
     """Check if the server is online or not."""
     return {"message": "API is live", "alive": True}
-
-
-@app.get("/blog/all", tags=["Blog"], response_description="List of all blogs")
-def all_blogs():
-    """Retrieve all blogs."""
-    return {"message": "All blogs"}
-
-
-@app.get("/blog/{id}", tags=["Blog"])
-def blog_with_id(id: int, response: Response):
-    """Retrieve a specific blog using id."""
-    if id > 5:
-        response.status_code = 404
-        return {"error": f"Blog with ID {id} NOT FOUND"}
-    return {"message": f"Blog with ID {id}"}
-
-
-class BlogType(str, Enum):
-    travel = "travel"
-    howto = "howto"
-    philosophy = "philosophy"
-
-
-@app.get("/blog/type/{type}", tags=["Blog"])
-def blogs_of_certain_type(type: BlogType):
-    """Retrieve blogs of a certain category"""
-    return {"message": f"All blogs of category {type}"}
-
-
-@app.get("blog/{id}/comment/{comment_id}", tags=["Blog", "Comment"])
-def get_comment(
-    id: int, comment_id: int, valid: bool = True, username: Optional[str] = ""
-):
-    """
-    Retrieve comment of a blog
-    - **id** mandatory blog id
-    - **comment_id** mandatory comment id
-    - **valid** optional valid boolean
-    - **username** optional username
-    """
-    return {
-        "message": f"blog_id {id}, comment_id {comment_id}, valid {valid} username {username}"
-    }
---
+++ routers/blog_get.py
@@ -0,0 +1,48 @@
+from typing import Optional
+from fastapi import APIRouter, Response, status
+from enum import Enum
+
+router = APIRouter(tags=["Blog"], prefix="/blog")
+
+
+@router.get("/all", response_description="List of all blogs")
+def all_blogs():
+    """Retrieve all blogs."""
+    return {"message": "All blogs"}
+
+
+@router.get("/{id}")
+def blog_with_id(id: int, response: Response):
+    """Retrieve a specific blog using id."""
+    if id > 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"error": f"Blog with ID {id} NOT FOUND"}
+    return {"message": f"Blog with ID {id}"}
+
+
+class BlogType(str, Enum):
+    travel = "travel"
+    howto = "howto"
+    philosophy = "philosophy"
+
+
+@router.get("/type/{type}")
+def blogs_of_certain_type(type: BlogType):
+    """Retrieve blogs of a certain category"""
+    return {"message": f"All blogs of category {type}"}
+
+
+@router.get("/{id}/comment/{comment_id}", tags=["Comment"])
+def get_comment(
+    id: int, comment_id: int, valid: bool = True, username: Optional[str] = ""
+):
+    """
+    Retrieve comment of a blog
+    - **id** mandatory blog id
+    - **comment_id** mandatory comment id
+    - **valid** optional valid boolean
+    - **username** optional username
+    """
+    return {
+        "message": f"blog_id {id}, comment_id {comment_id}, valid {valid} username {username}"
+    }
