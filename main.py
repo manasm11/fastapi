0482f59cb7a8d2206684801ddb9933163db8a160
Manas Mishra <manas.m22@gmail.com>
2022-07-06 05:47:17 +0530

    Created DbUser model

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from db import database, models
 from fastapi import FastAPI
 from routers import blog_get, blog_post
+
+models.Base.metadata.create_all(database.engine)
 
 app = FastAPI()
 app.include_router(blog_get.router)
